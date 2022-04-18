2d46413ce441e0a0c73a35432fd8e129807e3324
unknown <lucky4527@gmail.com>
2022-04-18 13:03:43 +0900

    2022/04/18

--- Sboard1/src/main/java/kr/co/Sboard1/controller/MainController.java
+++ Sboard1/src/main/java/kr/co/Sboard1/controller/MainController.java
@@ -1,12 +1,29 @@
 package kr.co.Sboard1.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import kr.co.Sboard1.vo.UserVo;
+
+@SessionAttributes("sessUser")
+@Controller
 public class MainController {
 
+	@ModelAttribute("sessUser")
+	public UserVo setUserVo() {
+		return null;
+	}
+	
 	@GetMapping(value={"", "/index"})
-	public String login() {
+	public String login(@ModelAttribute("sessUser") UserVo sessUser) {
+		// 로그인 체크
+		if(sessUser == null) {
+			return "redirect:/user/login?success=102";
+		}else {
+			return "forward:/article/list";
+		}
 		
-		return "/index";
 	}
 }
